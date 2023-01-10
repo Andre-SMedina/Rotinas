bfdc563a79887e32730d05b05f277afa94a8f3b6
André Luís Silva Medina <106404962+Andre-SMedina@users.noreply.github.com>
2023-01-10 11:37:55 -0300

    att

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,115 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+let banco = JSON.parse(localStorage.getItem("banco"));
+const btnSalvar = document.querySelector("#btnSalvar");
+const atividadesLista = document.querySelector("#atividadesLista");
+const iniciado = document.querySelector("#iniciado");
+const hora = document.querySelector("#hora");
+const dia = document.querySelector("#dia");
+const total = document.querySelector("#total");
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+async function bancoCreate() {
+  await localStorage.setItem(
+    "banco",
+    JSON.stringify({
+      atividades: [],
+      ativo: "",
+      historico: {
+        nadar: [
+          { data: "05/05/2020", inicio: "12:30", fim: "13:30", total: "" },
+        ],
+        correr: [],
+      },
+    })
+  );
+  banco = JSON.parse(localStorage.getItem("banco"));
 }
+
+if (!banco) {
+  bancoCreate();
+}
+
+function controle(id) {
+  const time = new Date();
+  const dias = ("0" + time.getDate()).slice(-2);
+  const mes = ("0" + (time.getMonth() + 1)).slice(-2);
+  const ano = time.getFullYear();
+  const dataAtual = `${dias}/${mes}/${ano}`;
+  const horaAtual = `${("0" + time.getHours()).slice(-2)}:${(
+    "0" + time.getMinutes()
+  ).slice(-2)}`;
+  const historico = banco.historico[id];
+
+  iniciado.innerText = `${id} iniciado`;
+  dia.innerText = `Dia: ${dataAtual}`;
+  hora.innerText = `Hora: ${horaAtual}`;
+
+  if (historico) {
+    historico.map((e) => {
+      if (e.data == dataAtual && !e.fim) {
+        //
+      }
+    });
+    historico.push({ data: dataAtual, inicio: horaAtual, fim: "", total: "" });
+    console.log(historico);
+  }
+}
+
+//adiciona as atividades na lista
+function addLista(id) {
+  const h3 = document.createElement("h3");
+  h3.innerText = id;
+  h3.classList.add("itens");
+  h3.setAttribute("id", id);
+  atividadesLista.appendChild(h3);
+  h3.addEventListener("click", async () => {
+    if (!banco.ativo) {
+      banco.ativo = id;
+      h3.classList.add("ativo");
+      controle(banco.ativo);
+    } else {
+      document.querySelector(`#${banco.ativo}`).classList.remove("ativo");
+      h3.classList.add("ativo");
+      banco.ativo = id;
+      controle(banco.ativo);
+    }
+    await localStorage.setItem("banco", JSON.stringify(banco));
+  });
+}
+
+//carrega as atividades cadastradas, no painel
+setTimeout(() => {
+  if (banco.atividades.length > 0) {
+    banco.atividades.map((e) => {
+      addLista(e);
+    });
+  }
+  if (banco.ativo) {
+    document.querySelector(`#${banco.ativo}`).classList.add("ativo");
+  }
+}, 300);
+
+//adiciona ou remove atividades quando clica no botão salvar
+btnSalvar.addEventListener("click", async () => {
+  const inputAtividades = document
+    .querySelector("#inputAtividades")
+    .value.toLowerCase();
+  const consulta = banco.atividades.find((e) => e == inputAtividades);
+
+  if (inputAtividades) {
+    if (consulta) {
+      banco.atividades = banco.atividades.filter(
+        (item) => item != inputAtividades
+      );
+      const remove = document.querySelector(`#${inputAtividades}`);
+      atividadesLista.removeChild(remove);
+      document.querySelector("#inputAtividades").value = "";
+      if (banco.ativo == inputAtividades) {
+        banco.ativo = "";
+      }
+    } else {
+      banco.atividades.push(inputAtividades);
+      addLista(inputAtividades);
+      document.querySelector("#inputAtividades").value = "";
+    }
+    await localStorage.setItem("banco", JSON.stringify(banco));
+  }
+});
